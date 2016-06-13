4374d2dcb86dedfa2d557f04bfb9a40ce7a81fa6
DonRog <losardor+git@gmail.com>
2016-06-13 14:34:00 +0200

    Versione funzionante di griditalia.py con funzione getAllHights

--- gridItalia.py
+++ gridItalia.py
@@ -30,30 +30,60 @@ class mapInfo():
 
 	def getHeight(self, position):
 		self.place=position
-		line=int((float(self.place[0])/DELTAS)*float(self.place[1])/DELTAS)-1
+		line=int((float(self.place[0])/DELTAS)*float(self.place[1])/DELTAS + float(place[0]/DELTAS))-1
 		self.height=gl.getline(self.theFile, line).split()[2]
 		return self.height
+
+	def getallHeights(self, grid, coarsnes):
+		listof_nodes=grid.nodes()
+		listof_nodes=[self.positionFromIndex(item, coarsnes) for item in listof_nodes]
+		lines=[int((float(place[0])/DELTAS)*float(place[1])/DELTAS + float(place[0]/DELTAS))-1 for place in listof_nodes]
+		linesdict = {lines[i]:i for i in range(len(lines))}
+		out = [0 for i in range(len(lines))]
+		values = linesdict.keys()
+		values.sort()
+		ind=0
+		for currentline, line in enumerate(open(self.theFile, "rU")):
+			if currentline == values[ind]:
+				out[linesdict[currentline]] = int(line.split()[2])
+				if(ind!=len(values)-1): 
+					ind+=1
+				else: break
+		return out
 
 	
 
 
-fraction=[]
-M = mapInfo("files/Grid.xyz")
-for coarsnes in range(1,7):
-	print "-- Map for coarsnes: " + str(coarsnes)
-	G = nx.grid_2d_graph(HEIGHT*coarsnes, WIDTH*coarsnes, True)
-	listOfNodes = G.nodes()
-	totalNum = len(listOfNodes)
-	for x in listOfNodes:
-		G.node[x]['position']=M.positionFromIndex(x, coarsnes)
-		G.node[x]['height']=M.getHeight(G.node[x]['position'])
-		print "--Node ({0}, {1}):\t {2}\t{3}\t{4}".format(x[0],
-		 x[1], G.node[x]['position'][0], G.node[x]['position'][1], 
-		 G.node[x]['height']) 
-	listOfNodes=[x for x in listOfNodes if(G.node[x]['height'] == 0)]
-	G.remove_nodes_from(listOfNodes)
-	fraction.append(len(G.nodes())/totalNum)
-print "For each coarsnes the respective fraction of existing nodes is:"
-print fraction
+def main():
+	fraction=[]
+	M = mapInfo("files/Grid.xyz")
+	for coarsnes in range(1,2):
+		print "#- Map for coarsnes: " + str(coarsnes)
+		G = nx.grid_2d_graph(HEIGHT*coarsnes, WIDTH*coarsnes, True)
+		listOfNodes = G.nodes()
+		totalNum = len(listOfNodes)
+		listOfHeights=M.getallHeights(G, coarsnes)
+		for x in listOfNodes:
+			G.node[x]['position']=M.positionFromIndex(x, coarsnes)
+		for x in range(len(listOfNodes)):
+			G.node[listOfNodes[x]]['height']=listOfHeights[x]
+		for x in listOfNodes:
+			print "--Node ({0}, {1}):\t {2}\t{3}\t{4}".format(x[0],
+			 x[1], G.node[x]['position'][0], G.node[x]['position'][1], 
+			 G.node[x]['height']) 
+		listOfNodes=[x for x in listOfNodes if int(G.node[x]['height']) == 0]
+		print listOfNodes
+		G.remove_nodes_from(listOfNodes)
+		len(G.nodes())
+		fraction.append(float(len(G.nodes()))/totalNum)
+	print "For each coarsnes the respective fraction of existing nodes is:"
+	print fraction
 
- 
+def test():
+	M=mapInfo("files/Grid.xyz")
+	coarsness=1
+	G = nx.grid_2d_graph(HEIGHT*coarsness, WIDTH*coarsness, True)
+	print M.getallHeights(G, coarsness)
+
+
+if __name__ == "__main__": main()
