8cbdec3b673fda055e9b2fcca8047f0c4c913ff9
DonRog <losardor+git@gmail.com>
2016-09-19 10:00:22 +0200

    Lest's sea

--- analisys2.py
+++ analisys2.py
@@ -11,29 +11,29 @@ for i in range(10):
 	y.append(B)
 fig=plt.figure()
 ax1=plt.subplot2grid((1,1),(0,0))
-colors = cm.rainbow(np.linspace(0, 1, len(y[0])))
-X=np.nditer(x)
-for Y in np.nditer(y):
-	plt.scatter(X, Y, marker='.', color=next(colors), label=str(float(i+1)/100))
+colors = cm.rainbow(np.linspace(0, 1, len(y)))
+X=list(x[0])
+i=0
+for Y in list(y):
+	plt.scatter(X, Y, marker='.', color=colors[i], label=str(float(i+1)/100))
+	i+=1
 plt.xlabel('Time Step')
 plt.ylabel('Number of Different Words')
 plt.title('Number of Different Words in Time')
 plt.legend()
 ax1.grid(True)
 plt.show()
-Y=[]
 last=[]
 max=[]
+betaas=[]
 for i in range(10):
-	Y[i]=nparray(y[i])
-	last[i]
-	max=Y[i].max()
-	beta=float(i+1)/100
-plt.plot(betaas,lasts, marker='*', color='r', label='the last ndw')
-plt.plot(betaas, maxes, marker='.', color='b', label='The max ndw')
+	last.append(y[i][-1])
+	max.append(y[i].max())
+	betaas.append(float(i+1)/100)
+plt.scatter(betaas,last, marker='*', color='r', label='the last ndw')
+#plt.scatter(betaas, max, marker='.', color='b', label='The max ndw')
 plt.xlabel('Beta')
 plt.ylabel('Number of different words')
 plt.title('The maximum and last value of the number of different words as a function of the weight beta')
 plt.legend()
 plt.show()
-
