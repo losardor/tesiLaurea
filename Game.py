1e2fecae716a86b6324cbe172f32f641a2a98784
DonRog <losardor+git@gmail.com>
2016-12-21 11:05:50 +0100

    added come comments

--- Game.py
+++ Game.py
@@ -1,4 +1,3 @@
-#def 
 import cPickle as pk
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -26,126 +25,137 @@ def Play(f, T=1000000, name="game.dat", prob=1):
     '''
 
 
-    name="files/Data/"+namegiving(name)
+    name = "files/Data/"+namegiving(name)
     target = open(name, "w")
-    clusterfile=open("clustering", "w")
+    clusterfile = open("clustering", "w")
 
-    #for x in range(len(time[::10])):
-    #    target.write(str(time[10*x])+"\t"+str(different_words[10*x])+"\t"+str(numberofWords[10*x])+"\n")
-    time=0
+    time = 0
     target.write(str(0)+"\t")
-    different_words=0
+    different_words = 0
     target.write(str(different_words)+"\t")
-    numberofWords=0
+    numberofWords = 0
     target.write(str(numberofWords)+"\n")
-    couples=[]
-    clustering=[]
-    seentimes=[100, 1000, 10000, 100000, 1000000, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000]
+    couples = []
+    clustering = []
+    seentimes=[100, 1000, 10000, 100000, 1000000, 1000000, 2000000, 3000000, 
+    4000000, 5000000, 6000000, 7000000, 8000000, 9000000]
     seentimes.append([t*10000000 for t in range(1,100)])
-    count=0
-    i=0
+    count = 0
+    i = 0
     while i<T:
         if i in seentimes and f.topology.tipo != COMPLETE:
             print i
-            clustersizefile=open("Clustersi"+str(i)+".dat", "w")
-            if SHOW==2:
+            clustersizefile = open("Clustersi"+str(i)+".dat", "w")
+            if SHOW == 2:
+                #Plot the image of the current grid
                 try:
-                    nodeColor=[]
+                    nodeColor = []
                     for x in f.topology.G.nodes():
                         if f.topology.G.node[x]['agent'].dict != []:
                             nodeColor.append(int(f.topology.G.node[x]['agent'].dict[0]))
                         else:
                             nodeColor.append(0)
-                    nodeColor=[float(color)/max(nodeColor) for color in nodeColor]
+                    nodeColor = [float(color)/max(nodeColor) for color 
+                    in nodeColor]
                 except:
                     print "the topolgy is complete and has no grid to print"
 
 
                 if f.topology.tipo == GRIDONMAP:
-                    fig=plt.figure(figsize=(16,12))
+                    fig = plt.figure(figsize=(16,12))
                     fig.patch.set_facecolor('#F8F8ff')
-                    ax=plt.subplot(111)
-                    elarge=[(u,v) for (u,v,d) in f.topology.G.edges(data=True) if d['weight'] >=0.05]
-                    esmall=[(u,v) for (u,v,d) in f.topology.G.edges(data=True) if d['weight'] <0.05]
+                    ax = plt.subplot(111)
+                    elarge = [(u,v) for (u,v,d) 
+                    in f.topology.G.edges(data=True) if d['weight'] >=0.05]
+                    esmall=[(u,v) for (u,v,d) 
+                    in f.topology.G.edges(data=True) if d['weight'] <0.05]
                     nx.draw_networkx_edges(f.topology.G, 
-                        pos={i:i for i in f.topology.G.nodes()}, edgelist=elarge, 
+                        pos={i:i for i in f.topology.G.nodes()}, 
+                        edgelist=elarge, 
                         width=1, alpha = 0.5)
                     nx.draw_networkx_edges(f.topology.G, 
-                        pos={i:i for i in f.topology.G.nodes()}, edgelist=esmall, 
+                        pos={i:i for i in f.topology.G.nodes()}, 
+                        edgelist=esmall, 
                         width=1, alpha=0.5,edge_color='b',style='dashed')
                     nx.draw_networkx_nodes(f.topology.G, 
-                        pos={i:i for i in f.topology.G.nodes()}, node_color=nodeColor, 
+                        pos={i:i for i in f.topology.G.nodes()}, 
+                        node_color=nodeColor, 
                         node_cmap=cm.Colormap("Accent"),
                         node_size=10)
                     plt.xlabel('X_grid identifier')
                     plt.ylabel('Y_grid identifier')
-                    plt.savefig("word_on_grid_time_"+str(i)) # display
-            lunghezze=[len(component) for component in sorted(word_clusters(f.topology), key=len, reverse=True)]
+                    plt.savefig("word_on_grid_time_"+str(i))
+            lunghezze=[len(component) for component 
+            in sorted(word_clusters(f.topology), key=len, reverse=True)]
             if len(lunghezze):
-                clustersizefile.writelines([str(lunghezza)+"\n" for lunghezza in lunghezze])
+                clustersizefile.writelines([str(lunghezza) + "\n" 
+                    for lunghezza in lunghezze])
                 if max(lunghezze)/30:
-                    binses=[grand for grand in range(0, max(lunghezze), max(lunghezze)/30)]
-                    fig5=plt.figure(figsize=(12,16))
+                    binses = [grand for grand 
+                    in range(0, max(lunghezze), max(lunghezze)/30)]
+                    fig5 = plt.figure(figsize=(12,16))
                     plt.hist(lunghezze, binses, histtype='bar', rwidth=0.8, 
                         label='cluster sizes')
-                    plt.savefig("Cluster_size:distribution_"+str(i))
+                    plt.savefig("Cluster_size:distribution_" + str(i))
                 clustering.append(float(sum(lunghezze))/len(lunghezze))
                 if len(lunghezze)<50 and count == 0:
-                    print "The number of clusters for time "+str(i)+"is close too the current distribution."
-                    count=count+1
+                    print ("The number of clusters for time " + str(i) + "is "
+                        "close too the current distribution.")
+                    count = count + 1
             else:
                 clustering.append(0)
             print clustering[-1], len(lunghezze)
-            clusterfile.write(str(i)+"\t"+str(clustering[-1])+"\t"+str(len(lunghezze))+"\n")
-
+            clusterfile.write(str(i) + "\t"+str(clustering[-1]) + "\t" 
+                + str(len(lunghezze)) + "\n")
             plt.close("all")
 
-
         [speaker, hearer] = f.Select()
-        if f.topology.tipo==COMPLETE:
+        if f.topology.tipo == COMPLETE:
         	couples.append([speaker.id,hearer.id])
-        if(hearer==None):
-            #print "Che rumore fa un albero che cade in una foresta disabitata?"
-            target.write(str(i+1)+"\t")
-            target.write(str(different_words)+"\t")
-            target.write(str(numberofWords)+"\n")
+        if hearer == None:
+            target.write(str(i+1) + "\t")
+            target.write(str(different_words) + "\t")
+            target.write(str(numberofWords) + "\n")
             continue
 
 
-        if(speaker==None):
-            print "che rumore fa il battito di una mano sola?"
-            target.write(str(i+1)+"\t")
+        if speaker == None:
+            print "Somehow the selected speaker doesn't exist: "+str(speaker)
+            target.write(str(i+1) + "\t")
             if speaker.ndw:
-                different_words=speaker.ndw
-                target.write(str(speaker.ndw)+"\t")
+                different_words = speaker.ndw
+                target.write(str(speaker.ndw) + "\t")
             else:
-                targe.write(str(different_words)+"\t")
-            target.write(str(numberofWords)+"\n")
+                targe.write(str(different_words) + "\t")
+            target.write(str(numberofWords) + "\n")
             continue
 
 
-        if DEBUG: 
-            print "speaker:", str(speaker.dict)," hearer:",str(hearer.dict)
+        if DEBUG:
+            print "speaker:", str(speaker.dict), " hearer:", str(hearer.dict)
 
-        coupleWords=len(speaker.dict)+len(hearer.dict)
+        coupleWords = len(speaker.dict) + len(hearer.dict)
         if(len(speaker.dict) == 1 and speaker.dict == hearer.dict): 
             # trivial case
-            #print "%d %d" %(i, speaker.ndw)
-            target.write(str(i+1)+"\t")
-            different_words=speaker.ndw
-            target.write(str(different_words)+"\t")
-            numberofWords+=len(speaker.dict)+len(hearer.dict)-coupleWords
-            target.write(str(numberofWords)+"\n")
-            continue
-        
 
-        if not speaker.dict: #if empty
+            target.write(str(i+1) + "\t")
+            different_words = speaker.ndw
+            target.write(str(different_words) + "\t")
+            numberofWords += len(speaker.dict) + len(hearer.dict) - coupleWords
+            target.write(str(numberofWords) + "\n")
+            continue
+
+        if not speaker.dict: 
+        #if empty
+
             w = speaker.NewWord()
             speaker.AddWord(w)
         w = speaker.ChooseWord()
-        if w in hearer.dict: # success
+        
+        if w in hearer.dict: 
+        # success
             if DEBUG: 
-                print "w:",str(w),"success"
+                print "w:", str(w), "success"
             if random.random() < prob:
                 speaker.EraseDict()
                 hearer.EraseDict()
@@ -153,36 +163,34 @@ def Play(f, T=1000000, name="game.dat", prob=1):
                 hearer.AddWord(w)
         else:
             if DEBUG: 
-                print "w:",str(w), "failure"
+                print "w:", str(w), "failure"
             hearer.AddWord(w)
 
-
-        target.write(str(i+1)+"\t")
-        different_words=speaker.ndw
+        target.write(str(i + 1) + "\t")
+        different_words = speaker.ndw
         target.write(str(different_words)+"\t")
-        numberofWords+=len(speaker.dict)+len(hearer.dict)-coupleWords
-        target.write(str(numberofWords)+"\n")
-        i+=1
-       
+        numberofWords += len(speaker.dict) + len(hearer.dict) - coupleWords
+        target.write(str(numberofWords) + "\n")
+        i += 1
     target.close()
 
-    differentWords=[]
-    numberWords=[]
+    differentWords = []
+    numberWords = []
     for k, line in enumerate(open(name, "r")):
-        if k%1000==0:
+        if k%1000 == 0:
             differentWords.append(int(line.split("\t")[1]))
             numberWords.append(int(line.split("\t")[2]))
 
     print len(differentWords), len(numberWords)
 
-    if SHOW==1:
-        if f.topology.tipo==COMPLETE:
-            x,y=zip(*couples)
+    if SHOW == 1:
+        if f.topology.tipo == COMPLETE:
+            x,y = zip(*couples)
             plt.scatter(x,y)
 
 
-        fig=plt.figure(figsize=(16,12))
-        ax=plt.subplot(111)
+        fig = plt.figure(figsize=(16,12))
+        ax = plt.subplot(111)
         ax.spines["top"].set_visible(False)
         ax.spines["bottom"].set_visible(False)
         ax.spines["right"].set_visible(False)
@@ -200,8 +208,8 @@ def Play(f, T=1000000, name="game.dat", prob=1):
         plt.legend()
         plt.show()
     
-        fig2=plt.figure(figsize=(16,12))
-        ax2=plt.subplot(111)
+        fig2 = plt.figure(figsize=(16,12))
+        ax2 = plt.subplot(111)
         ax2.spines["top"].set_visible(False)
         ax2.spines["bottom"].set_visible(False)
         ax2.spines["right"].set_visible(False)
@@ -218,34 +226,35 @@ def Play(f, T=1000000, name="game.dat", prob=1):
                 [len(f.topology.G.nodes()) for i in range(len(numberWords))], 
                 "--", lw=0.5, color="black", alpha=0.3)
         except:
-            plt.plot(range(len(numberWords)), [8100 for i in range(len(numberWords))], 
+            plt.plot(range(len(numberWords)), [8100 for i 
+                in range(len(numberWords))], 
                 "--", lw=0.5, color="black", alpha=0.3)
         plt.xlabel('Time Step')
         plt.ylabel('Number of Words')
         plt.show()
 
 
-        nodeColor=[]
+        nodeColor = []
         try:
             for x in f.topology.G.nodes():
                 if f.topology.G.node[x]['agent'].dict != []:
                     nodeColor.append(int(f.topology.G.node[x]['agent'].dict[0]))
                 else:
                     nodeColor.append(0)
-            nodeColor=[float(color)/max(nodeColor) for color in nodeColor]
+                nodeColor = [float(color)/max(nodeColor) for color in nodeColor]
         except:
             print "the topolgy is complete and has no grid to print"
 
         
         if f.topology.tipo == GRIDONMAP:
-            fig2=plt.figure()
-            elarge=[(u,v) for (u,v,d) in f.topology.G.edges(data=True)\
-             if d['weight'] >=0.05]
-            esmall=[(u,v) for (u,v,d) in f.topology.G.edges(data=True)\
-             if d['weight'] <0.05]
+            fig2 = plt.figure()
+            elarge = [(u,v) for (u,v,d) 
+            in f.topology.G.edges(data=True) if d['weight'] >= 0.05]
+            esmall=[(u,v) for (u,v,d) in f.topology.G.edges(data=True)
+            if d['weight'] <0.05]
             nx.draw_networkx_edges(f.topology.G, 
                 pos={i:i for i in f.topology.G.nodes()}, edgelist=elarge, 
-                width=1, alpha = 0.5)
+                width=1, alpha=0.5)
             nx.draw_networkx_edges(f.topology.G, 
                 pos={i:i for i in f.topology.G.nodes()}, edgelist=esmall,
                  width=1, alpha=0.5,edge_color='b',style='dashed')
@@ -254,36 +263,27 @@ def Play(f, T=1000000, name="game.dat", prob=1):
                 node_cmap=cm.Colormap("Accent"), node_size=20)
             plt.xlabel('X_grid identifier')
             plt.ylabel('Y_grid identifier')
-            plt.show() # display
-
+            plt.show()
 
     if f.topology.tipo == GRIDONMAP:
-        filename="final_grid_"+name
+        filename = "final_grid_" + name
         with open(filename, "wb") as output:
             pk.dump(f.topology.G, output, pk.HIGHEST_PROTOCOL)
     
     elif f.topology.tipo == COMPLETE:
         print "finished game\n"
-    
     else:
-        if SHOW==1:
-            fig2=plt.figure()
+        if SHOW == 1:
+            fig2 = plt.figure()
             nx.draw(f.topology.G, pos=nx.spring_layout(f.topology.G))
-            plt.show() # display
+            plt.show()
         
-    fig9=plt.figure()
+    fig9 = plt.figure()
     plt.plot(seentimes[:len(clustering)], clustering)
     plt.show()
 
 
     return (differentWords, numberWords)
-
-
-
-
-
-
-
 
 
 def namegiving(name):
@@ -294,34 +294,46 @@ def namegiving(name):
     
     if Path(name).is_file():
     	if name.endswith(')'):
-    		name=name[:-3]+'('+str(int(name[-2])+1)+')'
+    		name = name[:-3] + '(' + str(int(name[-2]) + 1) + ')'
     	else:
-    		name=name+'(2)'
+    		name = name + '(2)'
     	if Path(name).is_file():
     		return namegiving(name)
     return name
 
 def word_clusters(topology):
-    seen={}
+    '''
+    Finds clusters of agents using the same word and return a list of cluster
+    sizes
+    '''
+    seen = {}
     for v in topology.G:
         if v not in seen:
             if topology.G.node[v]['agent'].dict != []:
-                c=sp_length(topology, v, topology.G.node[v]['agent'].dict[0])
+                c = sp_length(topology, v, topology.G.node[v]['agent'].dict[0])
             else:
-                c={}
+                c = {}
             yield list(c)
             seen.update(c)
             
 def sp_length(topology, source, word):
-    seen={}
-    level=0
-    nextlevel={source:1}
+    '''
+    Finds clusters of agents using the same word and returns the list of nodes
+    in the cluster
+    '''
+
+
+    seen = {}
+    level = 0
+    nextlevel = {source:1}
     while nextlevel:
-        thislevel=nextlevel
-        nextlevel={}
+        thislevel = nextlevel
+        nextlevel = {}
         for v in thislevel:
             if v not in seen:
-                seen[v]=level
-                nextlevel.update({nbr:topology.G.node[nbr] for nbr in topology.G[v] if word in topology.G.node[nbr]['agent'].dict})
-        level=level+1
+                seen[v] = level
+                nextlevel.update({nbr:topology.G.node[nbr] for nbr 
+                    in topology.G[v] if word 
+                    in topology.G.node[nbr]['agent'].dict})
+        level = level+1
     return seen
--- NG.py
+++ NG.py
@@ -1,76 +1,49 @@
-#! /usr/bin/python
-
 import random
+import numpy as np
+import sys
+import getopt
 
 from Agent import *
 from Game import Play
-import numpy as np
-import sys, getopt
+
 
 def main(argv):
-	outputfile = ''
-	choice = 1
-	beta=0.05
-	prob=1
-	time=1000000000
-	try:
-		opts, args = getopt.getopt(argv,"ho:c:t:b:p:",["ofile=","choice=", "time=","beta=","prob="])
-	except getopt.GetoptError:
-		print 'NG.py -o <outputfile> -c <choice> -t <time> -p <probability>'
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print "NG.py -o <outputfile> -c <choice> -t <time> -b <beta>",
-			"\nfor choice the options are:\n0 for unweighted choice\n1 for weighted choice",
-			"for topology the possibilities are NON MI INTERESSA ADESSO",
-			"For beta interesting values are between 0.001 and 0.1"
+    outputfile = ''
+    choice = 1
+    beta = 0.05
+    prob = 1
+    time = 10000000
+    try:
+        opts, args = getopt.getopt(argv,"ho:c:t:b:p:",["ofile=","choice=",
+            "time=","beta=","prob="])
+    except getopt.GetoptError:
+        print 'NG.py -o <outputfile> -c <choice> -t <time> -p <probability>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("NG.py -o <outputfile> -c <choice> -t <time> -b <beta>\n"
+                "for choice the options are:\n0 for unweighted choice\n"
+                "1 for weighted choice\nFor topology the possibilities are: "
+                "NON MI INTERESSA ADESSO\n"
+                "For beta interesting values are between 0.001 and 0.1")
+            sys.exit()
+        elif opt in ("-c", "--choice"):
+            choice=arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-b", "--beta"):
+            beta=float(arg)
+        elif opt in ("-p", "--prob"):
+            prob=float(arg)
+        elif opt in ("-t", "--time"):
+            time=float(arg)
 
-			sys.exit()
-		elif opt in ("-c", "--choice"):
-			choice=arg
-		elif opt in ("-o", "--ofile"):
-			outputfile = arg
-		elif opt in ("-b", "--beta"):
-			beta=float(arg)
-		elif opt in ("-p", "--prob"):
-			prob=float(arg)
-		elif opt in ("-t", "--time"):
-			time=float(arg)
+    if outputfile == '':
+        outputfile = "gameBeta" + str(beta) + "Prob" + str(prob) + ".dat"
+    
+    folk = Folk(10000, GRID2D, choice=choice, Beta=beta)
+    Play(folk, time, name=outputfile, prob=prob)
 
-	if outputfile == '':
-		outputfile="gameBeta"+str(beta)+"Prob"+str(prob)+".dat"
-
-	folk=Folk(10000, GRIDONMAP, choice = choice, Beta = beta)
-	Play(folk, time, name=outputfile, prob=prob)
 
 if __name__=="__main__":
-	main(sys.argv[1:])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    main(sys.argv[1:])
